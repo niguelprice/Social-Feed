796dacbc5746a660004fe29af104ffcadad812b6
Niguel Price <niguelprice@yahoo.com>
2022-11-20 05:30:18 -0500

    created funtional displays for name and post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+
 
 function App() {
+
+const [entries, setEntries] = useState([{Name: 'Niguel Price', Post: 'Im a software engineer'}, {Name: 'Nigue Price', Post: 'Im a electrical engineer'}])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DisplayEntries parentEntries={entries} />
     </div>
   );
 }
---
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -0,0 +1,25 @@
+const DisplayEntries = (props) => {
+    return ( 
+        <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Post</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.parentEntries.map((entry) => {
+            return (
+              <tr>
+                <td>{entry.Name}</td>
+                <td>{entry.Post}</td>
+              </tr>
+            );
+          })}
+          
+        </tbody>
+      </table>
+     );
+}
+ 
+export default DisplayEntries;
